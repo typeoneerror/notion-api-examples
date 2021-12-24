d08512e9a53f203bf7cadb24120074eb0fcd41cf
Benjamin Borowski <ben@weareokidoki.com>
2021-12-23 18:33:36 -0800

    Use titleDate util in createFromTemplate example

--- examples/pages/create-from-template/index.js
+++ examples/pages/create-from-template/index.js
@@ -1,16 +1,13 @@
 const { yargs } = require('../../shared');
 const { createFromTemplate } = require('../../shared/create-from-template');
-const props = require('../../shared/props');
+const titledDate = require('../../shared/titled-date');
 const { log } = require('../../shared/utils');
 
 const templateId = 'da29caf7fb84440f80f2a0ed9804b6e7';
 const argv = yargs.default({ templateId }).argv;
 
 (async () => {
-  const page = await createFromTemplate(argv.templateId, {
-    Name: props.title('Duty Crew: Dec 23, 2021'),
-    Date: props.date('2021-12-23'),
-  });
+  const page = await createFromTemplate(argv.templateId, titledDate('Duty Crew'));
 
   log(page);
 })();
