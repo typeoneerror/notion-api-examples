aaee9295b6519d5b3298dc6a352c491c1ef7d734
Benjamin Borowski <ben@weareokidoki.com>
2022-01-21 12:08:32 -0800

    wip

--- examples/pages/create-from-template/index.js
+++ examples/pages/create-from-template/index.js
@@ -3,7 +3,7 @@ const { createFromTemplate } = require('../../shared/create-from-template');
 const titledDate = require('../../shared/titled-date');
 const { log } = require('../../shared/utils');
 
-const templateId = 'da29caf7fb84440f80f2a0ed9804b6e7';
+const templateId = '2aff60e00a314bffb1a53971c9a756ff';
 const argv = yargs.default({ templateId }).argv;
 
 (async () => {
