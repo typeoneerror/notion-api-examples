794ce333629723bdc469a62cd1b25542c3a9efd9
Benjamin Borowski <ben@weareokidoki.com>
2022-05-26 14:04:09 -0700

    Improves multi-select sorting
    - Add case sensitive option
    - Actually use yargs w/database and sort prop setting

--- examples/databases/sort-multi-select/index.js
+++ examples/databases/sort-multi-select/index.js
@@ -3,26 +3,29 @@ const { log } = require('../../shared/utils');
 const _ = require('lodash');
 
 const databaseId = 'f91e66f29d63457894be7c91b132f345';
-const argv = yargs.default({ databaseId }).argv;
+const sortProp = 'Feeling';
+const argv = yargs
+  .boolean('case-sensitive')
+  .default({ databaseId, sortProp, caseSensitive: true }).argv;
 
 (async () => {
   let database = await notion.databases.retrieve({
-    database_id: databaseId,
+    database_id: argv.databaseId,
   });
 
-  const sortProp = 'Feeling';
-  const propId = database.properties[sortProp].id;
+  const propId = database.properties[argv.sortProp].id;
+  const iteratee = argv.caseSensitive ? 'name' : [(option) => option.name.toLowerCase()];
 
   // Sort the options and remove color since it cannot be updated via API
   const sortedOptions = _.map(
-    _.orderBy(database.properties[sortProp].multi_select.options, 'name'),
+    _.orderBy(database.properties[argv.sortProp].multi_select.options, iteratee),
     (option) => {
       return _.omit(option, 'color');
     }
   );
 
   const properties = {
-    database_id: databaseId,
+    database_id: argv.databaseId,
     properties: {
       [propId]: {
         multi_select: {
@@ -34,5 +37,5 @@ const argv = yargs.default({ databaseId }).argv;
 
   database = await notion.databases.update(properties);
 
-  log(database);
+  log(database.properties[argv.sortProp]);
 })();
