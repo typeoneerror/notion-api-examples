52db98906c3ae88cc49e31c8715732203296f7c4
Benjamin Borowski <ben@weareokidoki.com>
2026-03-27 19:15:12 -0700

    Improves property retrieval ex

--- examples/properties/retrieve.js
+++ examples/properties/retrieve.js
@@ -1,19 +1,28 @@
 /**
- * Retrieve a property _value_ from a database.
+ * Retrieve a property _value_ from a page.
  *
  * Required if you want to get accurate data from Rollups via the API.
  *
  * Arguments:
  *
+ * --page-id: ID of the page to fetch property from
  * --prop-id: ID of the property to fetch
  */
 
 const { notion, yargs } = require('../shared');
 const { log } = require('../shared/utils');
 
-const pageId = 'a2d67b9cb48e4b2aaca6026d8d577dfd';
-const propId = 'uy%7Db';
-const argv = yargs.default({ pageId, propId }).argv;
+const pageId = '3291c1cce3f380b7b2b0d382d589ef78';
+const propId = '}Ulu';
+const argv = yargs
+  .option('pageId', {
+    alias: 'd',
+    default: pageId,
+  })
+  .option('propId', {
+    alias: 'p',
+    default: propId,
+  }).argv;
 
 (async () => {
   const prop = await notion.pages.properties.retrieve({
